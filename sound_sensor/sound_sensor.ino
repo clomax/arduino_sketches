8882a80a0f8d58a8e6fc3e05b8f36d2a17d4df44
Craig Lomax <craig@clomax.me.uk>
2017-01-08 00:00:45 +0000

    Sound sensor.

---
+++ sound_sensor/sound_sensor.ino
@@ -0,0 +1,35 @@
+int led_pins[5] = {8,7,6,5,4};
+int values[5] = {};
+int MAX_SND = 100;
+float led_min_time_on = 250.0;
+float leds_on[5] = {};
+
+void setup()
+{
+    Serial.begin(9600);
+    for(int i=0; i<5; i++)
+    {
+        pinMode(led_pins[i], OUTPUT);
+        values[i] = MAX_SND / 6 * (i+1);
+    }
+}
+
+void loop()
+{
+    int sensorValue = analogRead(A0);
+
+    for(int i=0; i<5; i++)
+    {
+        if (sensorValue > values[i])
+        {
+            digitalWrite(led_pins[i], HIGH);
+            leds_on[i] = millis();
+        }
+
+        if (leds_on[i] < millis() - led_min_time_on)
+            digitalWrite(led_pins[i], LOW);
+    }
+
+    Serial.println(sensorValue);
+    delay(30);
+}
